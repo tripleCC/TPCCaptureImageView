3d1ac5c5be6afb228c4127473a7a78447d87fbfa
tripleCC <562327931@qq.com>
2015-06-20 15:09:07 +0800

    Initial Commit

---
+++ TPCCaptureImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TPCCaptureImageView
+//
+//  Created by 宋瑞旺 on 15/6/20.
+//  Copyright (c) 2015年 tripleCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TPCCaptureImageView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TPCCaptureImageView
+//
+//  Created by 宋瑞旺 on 15/6/20.
+//  Copyright (c) 2015年 tripleCC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
